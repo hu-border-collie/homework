8eeddc4f7164c434aa17a55991dd1c96fd333287
hu-border-collie <hu.border_collie@outlook.com>
2025-11-27 22:01:58 +0800

    11/27/22/01

---
+++ c-demo/9-6-喵.c
@@ -0,0 +1,81 @@
+// 头文件喵
+#define 喵头一 <stdio.h>
+#define 喵头二 <string.h>
+#include 喵头一
+#include 喵头二
+
+// 关键字喵
+#define 喵数   int
+#define 喵字   char
+#define 喵主   main
+#define 喵开   {
+#define 喵关   }
+
+#define 喵圈   for
+#define 喵若   if
+#define 喵返   return
+
+// 标点符号喵
+#define 喵左    (
+#define 喵右    )
+#define 喵左中  [
+#define 喵右中  ]
+#define 喵句    ;
+#define 喵分    ;
+#define 喵逗    ,
+
+// 运算符喵
+#define 喵是    =
+#define 喵小    <
+#define 喵大等  >=
+#define 喵小等  <=
+#define 喵与    &&
+#define 喵步    ++
+#define 喵减    -
+
+// 数字喵
+#define 喵零    0
+#define 喵五十  50
+#define 喵九七  97
+#define 喵三二  32
+#define 喵一二二 122
+
+// 函数名喵
+#define 喵印    printf
+#define 喵取串  gets
+#define 喵出    puts
+#define 喵长    strlen
+
+// 文本喵
+#define 喵提示  "Enter a string:"
+
+// 变量名喵
+#define 喵串    ch
+#define 喵喵喵  i
+
+喵数 喵主 喵左 喵右
+喵开
+    喵字 喵串 喵左中 喵五十 喵右中 喵句
+
+    喵印 喵左 喵提示 喵右 喵句
+    喵取串 喵左 喵串 喵右 喵句
+
+    喵圈 喵左 喵数 喵喵喵 喵是 喵零 喵分
+          喵喵喵 喵小 喵长 喵左 喵串 喵右 喵分
+          喵喵喵 喵步
+        喵右
+    喵开
+        喵若 喵左
+              喵串 喵左中 喵喵喵 喵右中 喵大等 喵九七
+              喵与
+              喵串 喵左中 喵喵喵 喵右中 喵小等 喵一二二
+             喵右
+        喵开
+            喵串 喵左中 喵喵喵 喵右中 喵是
+                喵串 喵左中 喵喵喵 喵右中 喵减 喵三二 喵句
+        喵关
+    喵关
+
+    喵出 喵左 喵串 喵右 喵句
+    喵返 喵零 喵句
+喵关
